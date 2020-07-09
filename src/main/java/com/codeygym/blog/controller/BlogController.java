853aebef6a5eb5a87aba857913d37c4f18bd6ddb
= <=>
2020-07-09 16:16:34 +0700

    fix deploy heroku

--- src/main/java/com/codeygym/blog/controller/BlogController.java
+++ src/main/java/com/codeygym/blog/controller/BlogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class blogController {
+public class BlogController {
 
     @Autowired
     private PostService postService;
